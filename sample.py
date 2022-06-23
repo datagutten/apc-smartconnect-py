37cab58d83d5734b86a9acd42d363cad4a1cdd50
datagutten <datagutten@datagutten.net>
2022-06-23 21:41:30 +0200

    Add sample script

---
+++ sample.py
@@ -0,0 +1,22 @@
+import sys
+from pprint import pprint
+
+import requests
+
+from apc_smartconnect import APCSmartConnect
+
+smart = APCSmartConnect('cookies.lwp')
+
+# Try to query using saved cookies
+try:
+    gateways = smart.gateways()
+except requests.exceptions.HTTPError as e:
+    if len(sys.argv) < 3:
+        print('Usage: sample.py [username] [password]')
+        exit(1)
+
+    smart.login(sys.argv[1], sys.argv[2])
+    gateways = smart.gateways()
+
+for gateway in gateways:
+    pprint(smart.gateway_info(gateway))

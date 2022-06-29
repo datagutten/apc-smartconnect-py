8b34560f1bed5cdd75e9f672eb11ac98b08da716
datagutten <datagutten@datagutten.net>
2022-06-29 16:46:27 +0200

    Simplify sample.py, saved cookies did not work

--- sample.py
+++ sample.py
@@ -1,22 +1,15 @@
 import sys
 from pprint import pprint
 
-import requests
-
 from apc_smartconnect import APCSmartConnect
 
+if len(sys.argv) < 3:
+    print('Usage: sample.py [username] [password]')
+    exit(1)
+
 smart = APCSmartConnect()
+smart.login(sys.argv[1], sys.argv[2])
 
-# Try to query using saved cookies
-try:
-    gateways = smart.gateways()
-except requests.exceptions.HTTPError as e:
-    if len(sys.argv) < 3:
-        print('Usage: sample.py [username] [password]')
-        exit(1)
-
-    smart.login(sys.argv[1], sys.argv[2])
-    gateways = smart.gateways()
-
+gateways = smart.gateways()  # Fetch all gateways (UPSs)
 for gateway in gateways:
     pprint(smart.gateway_info(gateway))
